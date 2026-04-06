d403f2038be14c1ec19afb5abc609e3b0e40fcef
sagarnaphade-lang <sagarnaphade025@gmail.com>
2026-04-06 22:28:07 +0530

    Delete Assn4/q7.py

--- Assn4/q7.py
+++
@@ -1,8 +0,0 @@
-# 7. WAP to print all integers upto n that aren’t divisible by 2 and 3.
-print('Program to print all integers upto n that aren’t divisible by 2 and 3.')
-n = int(input("Enter n: "))
-
-
-for i in range(1,n+1,1):
-    if(i%6!=0):
-        print(i,end=' ')
