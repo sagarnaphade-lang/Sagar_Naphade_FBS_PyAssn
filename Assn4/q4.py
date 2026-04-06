b9a47ea7765da8ed9c314f97cc90e9a4ea8640df
sagarnaphade-lang <sagarnaphade025@gmail.com>
2026-04-06 22:27:03 +0530

    Delete Assn4/q4.py

--- Assn4/q4.py
+++
@@ -1,9 +0,0 @@
-# 4. WAP to print factorial of a number .
-print('Program to print factorial of a number .')
-
-n = int(input("Enter n: "))
-facto = 1
-for i in range(1, n+1):
-    facto *= i
-
-print("factorial of given number =", facto)
